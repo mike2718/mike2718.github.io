0e6f5d8ce9732d63453267aa248a8d62641e467b
Mike Akiba <mike2718@gmail.com>
2020-05-14 01:53:00 +0800

    不得已更新

--- tidy.sh
+++ tidy.sh
@@ -4,4 +4,4 @@ tidy -utf8 -m 404.html Chrome浏览器快捷键.html \
 	index.html Linux命令行工具.html \
 	Swarm贴纸攻略.html 杀戮的天使游戏攻略.html \
 	心夜：静电茧游戏攻略.html 一个人在家时II游戏攻略.html \
-	自编译软件.html 惧魔症候群游戏攻略.html 奇怪的电话游戏攻略.html
+	奇怪的电话游戏攻略.html
